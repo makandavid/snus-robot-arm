798058c183b52daa7b171547cbe7516949b016f2
David Makan <davemakan@gmail.com>
2025-10-22 13:39:40 +0200

    Added initial default sqlite connection

---
+++ RobotArmServer/Data/DatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data.SQLite;
+
+namespace RobotArmServer.Data
+{
+    public class DatabaseHelper
+    {
+        private static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public static SQLiteConnection GetConnection()
+        {
+            var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
