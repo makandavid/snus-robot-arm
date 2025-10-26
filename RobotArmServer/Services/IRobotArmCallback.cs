fd3f27effd82e91e6440031f9c64e0a282212978
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-10-26 19:39:35 +0100

    Create IRobotArmCallback interface
    Serves to notify clients of state changes

---
+++ RobotArmServer/Services/IRobotArmCallback.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using RobotArmServer.Models;
+
+namespace RobotArmServer.Services
+{
+    [ServiceContract]
+    public interface IRobotArmCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnStateChanged(RobotArmState state);
+    }
+}
