1069be09c2384ea5647d57e0c5287b1de835cf76
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-10-26 19:29:26 +0100

    Create new models

---
+++ RobotArmServer/Models/CommandRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RobotArmServer.Models
+{
+    public class CommandRequest : IComparable<CommandRequest>
+    {
+        public string ClientName { get; set; }
+        public string Command { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TaskCompletionSource<CommandResult> CompletionSource { get; set; }
+        
+        private int Priority => ClientName switch
+        {
+            "Client1" => 1,
+            "Client2" => 2,
+            "Client3" => 2,
+            _ => 999
+        };
+        
+        public int CompareTo(CommandRequest other)
+        {
+            int priorityCompare = Priority.CompareTo(other.Priority);
+            return priorityCompare != 0 ? priorityCompare : Timestamp.CompareTo(other.Timestamp);
+        }        
+    }
+}
---
+++ RobotArmServer/Models/RobotArmState.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization;
+
+namespace RobotArmServer.Models
+{
+    [DataContract]
+    public class RobotArmState
+    {
+        [DataMember] public int X { get; set; }
+
+        [DataMember] public int Y { get; set; }
+
+        [DataMember] public int Angle { get; set; }
+
+        public RobotArmState()
+        {
+        }
+
+        public RobotArmState(RobotArm arm)
+        {
+            X = arm.X;
+            Y = arm.Y;
+            Angle = arm.Angle;
+        }
+
+        public RobotArmState(int x, int y, int angle)
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+
+        public override string ToString()
+        {
+            return $"Position=({X},{Y}), Rotation={Angle}°";
+        }
+    }
+}
